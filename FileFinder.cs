ff4e414a4ca5506fdd41a129aeda1c3f3490d362
= <alexander.hilmar@gmail.com>
2014-08-10 03:28:29 +0000

    initial commit

---
+++ FileFinder.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace LineCounter
+{
+    class FileFinder
+    {
+        public List<string> files = new List<string>();
+        public string path { get; set; }
+        private List<string> extensions = new List<string>();
+        private bool subDirectories = false;
+        
+
+        public FileFinder(string _path, List<string> _extensions, bool _subDirectories = false)
+        {
+            path = _path;
+            extensions = _extensions;
+            subDirectories = _subDirectories;
+         
+            try
+            {
+                findAndReadFiles(path);
+                if (subDirectories) findFolders(path);
+            }
+            catch (IOException e)
+            {
+                //do something with error
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        private void findAndReadFiles(string path)
+        {
+            foreach (string file in Directory.GetFiles(path))
+            {
+                if (extensions.Contains(Path.GetExtension(file)))
+                {
+                    files.Add(file);
+                }
+            }
+        }
+     
+        private void findFolders(string path)
+        {
+            foreach (string folder in Directory.GetDirectories(path))
+            {
+                findAndReadFiles(folder);
+                findFolders(folder);
+            }
+        }
+
+        public int countFiles
+        {
+            get { return files.Count(); }   
+        }
+
+        public int fileLines(string filePath)
+        {
+            return File.ReadAllLines(filePath).Count();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LineCounter
+{
+    class Program
+    {
+        /* EXAMPLE USAGE */
+        static void Main(string[] args)
+        {
+            FileFinder filefinder = new FileFinder("C:/xampp/htdocs/bobo_gru/public/js", new List<string> { ".js" }, true);
+
+            foreach (string item in filefinder.files)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
